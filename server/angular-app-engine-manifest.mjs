fe29c113ea40d185acc045c375832233ae991be1
Gio-Mtz <inggiomtzg@gmail.com>
2025-07-19 16:34:36 -0600

    Auto-generated commit

---
+++ server/angular-app-engine-manifest.mjs
@@ -0,0 +1,10 @@
+
+export default {
+  basePath: 'https://Gio-Mtz.github.io/Gio-Mtz.github.io',
+  supportedLocales: {
+  "en-US": ""
+},
+  entryPoints: {
+    '': () => import('./main.server.mjs')
+  },
+};
